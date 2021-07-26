961e051224a5d0af5ef1aa3002e6977b30ca686b
jamina17 <jabdinbekova@hotmail.com>
2021-07-26 11:53:50 -0500

    Test case is all done

--- src/ResetPractice.java
+++ src/ResetPractice.java
@@ -3,4 +3,8 @@ public class ResetPractice {
     //Test case part1 is done
     //Test case part2 is done
     //Test case part3 is done
+    //Test case part4 is done
+    //Test case part5 is done
+    //Mixed reset remove the commit from history but keep the local changes
+    //Did part 4 and 5 and I amd done in one commit
 }
